ad4e0cf3e4fbd6f58d910c1d3682eabdf8c82306
Andrew Kirwin <andrew.kirwin@gmail.com>
2017-12-14 14:24:15 +0000

    initial commit

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-simple-sortable'
+  name: 'ember-simple-sortable',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('node_modules/sortablejs/Sortable.js');
+  }
 };
