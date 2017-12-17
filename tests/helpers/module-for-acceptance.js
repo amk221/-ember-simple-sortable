a3161d654668ec071573eb25a1503d2f0411958b
Andrew Kirwin <andrew.kirwin@gmail.com>
2017-12-17 13:47:34 +0000

    fix tests

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,7 +1,9 @@
+/* eslint-disable max-len */
+
 import { module } from 'qunit';
-import { resolve } from 'rsvp';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
+import { Promise } from 'rsvp';
 
 export default function(name, options = {}) {
   module(name, {
@@ -14,8 +16,8 @@ export default function(name, options = {}) {
     },
 
     afterEach() {
-      let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
-      return resolve(afterEach).then(() => destroyApp(this.application));
+      const afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+      return Promise.resolve(afterEach).then(() => destroyApp(this.application));
     }
   });
 }
--- tests/integration/components/x-sortable-test.js
+++ tests/integration/components/x-sortable-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('sortable-parent', 'Integration | Component | sortable parent', {
+moduleForComponent('x-sortable', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{sortable-parent}}`);
+  this.render(hbs`{{x-sortable}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#sortable-parent}}
+    {{#x-sortable}}
       template block text
-    {{/sortable-parent}}
+    {{/x-sortable}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
